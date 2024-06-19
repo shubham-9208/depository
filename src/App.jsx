985b210349cc0281039d52d0620cc07011a31a30
shubham-9208 <shubham.k.m9208@gmail.com>
2024-06-19 10:36:01 +0530

    Initial Upload

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+
+import Hero from "./Component/Hero"
+import Navbar from "./Component/Navbar"
+import Skill from "./Component/Skill"
+import Project from './Component/Project';
+import Contect from "./Component/Contact";
+import Footer from "./Component/Footer";
+import { useState } from "react";
+
+
+
+
+
+
+function App() {
+
+
+
+  const [sideMenuOpen, setsideMenuOpen] = useState(false)
+
+
+  return (
+    <>
+      <div className=" class text-neutral-300 selection:bg-white selection:text-black  ">
+
+        <div className=" fixed top-0 -z-10 h-full w-full overflow-x-hidden">
+          <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+        </div>
+        <div className=" relative mx-auto lg:px-[140px] px-4 pb-8">
+          <Navbar
+            sideMenuOpen={sideMenuOpen}
+            setsideMenuOpen={setsideMenuOpen}
+          />
+          <Hero  />
+          <Skill  />
+          <Project  sideMenuOpen={sideMenuOpen} />
+          <Contect  />
+          <Footer />
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base:"/depository/",
+  plugins: [react()],
+  define:{
+    'process.env.VITE_API_KEY':JSON.stringify(process.env.VITE_API_KEY)
+  }
+})
